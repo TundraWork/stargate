375039cdaca9abf0c33e4e176ac1cf473a6e9790
Tundra Work <tundrawork@gmail.com>
2025-02-14 20:26:59 +1100

    feat: update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 BIN_FILENAME=stargate
 mkdir -p output/bin
+mkdir -p output/docs
+cp docs/* output/docs 2>/dev/null
 cp script/* output 2>/dev/null
 chmod +x output/bootstrap.sh
 go build -o output/bin/${BIN_FILENAME}
